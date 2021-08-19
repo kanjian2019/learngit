9d9a92d37f1576d1806d973b70e1864fe70923dd
kangjianqiang1 <kangjianqiang1@jd.com>
2021-08-19 16:16:23 +0800

    添加了add方法

--- code/src/com/test/main/Main.java
+++ code/src/com/test/main/Main.java
@@ -3,5 +3,14 @@ package com.test.main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        /**
+         * 这是注释
+         */
+        System.out.println("world");
+        System.out.println(add(2, 6));
+    }
+
+    public static int add(int a, int b){
+        return a + b;
     }
 }
